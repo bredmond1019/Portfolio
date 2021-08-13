58af66bc34d367aee2c52bdbba0236a93c7040b4
bredmond1019 <bredmond1019@penguin>
2021-08-13 15:36:31 -0400

    Updated Repository

--- Fala/fala/models.py
+++ Fala/fala/models.py
@@ -1,17 +1,25 @@
 from fala import db
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(64), unique=True, index=True)
 
-    def __repr__(self):
-        return f"<User {self.email}"
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
 
+    users = db.relationship("User", backref='role')
+
     def __repr__(self):
         return f'<Role {self.name}>'
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True, index=True)
+
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return f"<User {self.email}"
