12a31f6a613883464efe6c8b6e24d4f18ad4c335
Brandon <bredmond1019@gmail.com>
2021-08-31 17:56:05 -0400

    Basic Webpack App with Babel

--- Webpack App/webpack.config.js
+++ Webpack App/webpack.config.js
@@ -1,47 +1,29 @@
-const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
-let mode = "development";
-
-if (process.env.NODE_ENV === "production") {
-  mode = "production";
-}
 
 module.exports = {
-  mode: mode,
-
-  entry: {
-    main: path.resolve(__dirname, "./src/index.js"),
-  },
-
+  mode: "development",
+  entry: __dirname + "/src/index.js",
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+    filename: "bundle.js",
   },
-
   module: {
     rules: [
-      // JavaScript
       // {
-      //   test: /\.(js|jsx)?/,
-      //   exclude: /node_modules/,
-      //   use: "babel-loader",
+      //   test: /\.scss$/,
+      //   use: ["style-loader", "css-loader", "sass-loader"]
       // },
-      // {
-      //   test: /\.(png|jpe?g|gif|svg)$/i,
-      //   type: "asset/resource",
-      // },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
   },
-
   plugins: [
     new HtmlWebpackPlugin({
-      title: "webpack Boilerplate",
-      template: path.resolve(__dirname, "./src/template.html"), // template file
-      filename: "index.html", // output file
+      template: path.resolve(__dirname, "src", "index.html"),
     }),
-    new CleanWebpackPlugin(),
   ],
 };
