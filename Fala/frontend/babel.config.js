395d4a546a843211c93a4cb940db9342476152bd
bredmond1019 <bredmond1019@penguin>
2021-09-06 22:15:52 -0400

    Need to figure out what babel loader isnt functioning properly

--- Fala/frontend/babel.config.js
+++ Fala/frontend/babel.config.js
@@ -1,12 +1,13 @@
 module.exports = {
 	presets: [
-		["@babel/preset-react", { runtime: "automatic" }],
+		["@babel/preset-react"], // , { runtime: "automatic" }
 		"@babel/preset-env",
 	],
+	// assumptions: {
+	// 	setPublicClassFields: true,
+	// },
 	plugins: [
-		"@babel/plugin-proposal-decorators",
-		{
-			legacy: true,
-		},
+		["@babel/plugin-proposal-decorators", { legacy: true }],
+		["@babel/plugin-proposal-class-properties"],
 	],
 };
