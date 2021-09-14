eb45f746335a12b3bf6846f610ce3af6c0cf1490
bredmond1019 <bredmond1019@gmail.com>
2021-09-14 15:59:45 -0400

    Created Review WORD TILES with IMAGE & TRANSLATION

--- Fala/frontend/src/components/Words.jsx
+++ Fala/frontend/src/components/Words.jsx
@@ -7,9 +7,9 @@ export default function Words(props) {
         props.words.map((word, i) => {
           return (
             <div className="word-tile" key={i}>
-              <h2>{word.word}</h2>
-              <h3>{word.translation}</h3>
-              <img src={word.image} alt="" />
+              <h2 className='expression'>{word.word}</h2>
+              <h3 className="translation">{word.translation}</h3>
+              <img src={word.image} alt="" className="expression-image"/>
             </div>
           );
         })}
