d6045037a712814fca5f673abe3cd592589b09a0
bredmond1019 <bredmond1019@gmail.com>
2022-02-13 15:35:56 -0500

    Added Roles Query -- GQL

--- Climbr/climbr/graphql/objects.py
+++ Climbr/climbr/graphql/objects.py
@@ -1,11 +1,19 @@
 import graphene
 from graphene import relay
-from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from graphene_sqlalchemy import SQLAlchemyObjectType
 
-from ..models import User as UserModel
+
+from ..models import User as UserModel, Role as RoleModel
 
 
 class UserObject(SQLAlchemyObjectType):
     class Meta:
         model = UserModel
+        interfaces = (relay.Node, )
+        exclude_fields = ("role_id")
+
+
+class RoleObject(SQLAlchemyObjectType):
+    class Meta:
+        model = RoleModel
         interfaces = (relay.Node, )
--- Climbr/climbr/graphql/query.py
+++ Climbr/climbr/graphql/query.py
@@ -1,10 +1,10 @@
-import email
+
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField
 
-from ..models import User as UserModel
-from ..graphql.objects import UserObject as User
+from ..models import User as UserModel, Role as RoleModel
+from ..graphql.objects import UserObject as User, RoleObject as Role
 
 
 class Query(graphene.ObjectType):
@@ -13,12 +13,23 @@ class Query(graphene.ObjectType):
     users = graphene.List(
         lambda: User, email=graphene.String())
 
+    roles = graphene.List(
+        lambda: Role, name=graphene.String()
+    )
+
     def resolve_users(self, info, email=None):
         query = User.get_query(info)
-        print(query)
-        print(info)
+
         if email:
             query = query.filter(UserModel.email == email)
         return query.all()
 
+    def resolve_roles(self, info, name=None):
+        query = Role.get_query(info)
+
+        if name:
+            query = query.filter(RoleModel.name == name)
+        return query.all()
+
     all_users = SQLAlchemyConnectionField(User.connection)
+    all_roles = SQLAlchemyConnectionField(Role.connection)
