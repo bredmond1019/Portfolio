89238cf7c2c71cfc37d2cd70dcf8093b49b3a247
bredmond1019 <bredmond1019@gmail.com>
2022-03-09 19:28:14 -0500

    Updated Protected Profile Query

--- Climbr/climbr/graphql/query.py
+++ Climbr/climbr/graphql/query.py
@@ -2,6 +2,9 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField
+from flask_graphql_auth import query_header_jwt_required
+
+from climbr.graphql.mutations import ProtectedProfile
 
 from ..models import Profile as ProfileModel, \
     User as UserModel, \
@@ -9,8 +12,9 @@ from ..models import Profile as ProfileModel, \
 
 from ..graphql.objects import UserObject as User, \
     RoleObject as Role, \
-    ProfileObject as Profile \
-    # SkillObject as Skill
+    ProfileObject as Profile
+
+from ..graphql.mutations import ProtectedProfile
 
 
 class Query(graphene.ObjectType):
@@ -23,9 +27,11 @@ class Query(graphene.ObjectType):
         lambda: Role, name=graphene.String()
     )
 
-    profiles = graphene.List(
-        lambda: Profile, id=graphene.Int()
-    )
+    # profiles = graphene.List(
+    #     lambda: Profile, id=graphene.Int()
+    # )
+    profiles = graphene.Field(
+        type=ProtectedProfile, token=graphene.String(), id=graphene.Int())
 
     def resolve_users(self, info, email=None):
         query = User.get_query(info)
@@ -41,12 +47,14 @@ class Query(graphene.ObjectType):
             query = query.filter(RoleModel.name == name)
         return query.all()
 
+    @query_header_jwt_required
     def resolve_profiles(self, info, id=None):
         query = Profile.get_query(info)
 
         if id:
             query = query.filter(
-                ProfileModel.id == id)
+                ProfileModel.id == id).first()
+            return query
         return query.all()
 
     all_users = SQLAlchemyConnectionField(User.connection)
